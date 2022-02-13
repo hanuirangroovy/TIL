2f90486d5caec48ec8bbeb82aa507727a26f4e85
haru <jiaiha@naver.com>
2022-02-13 23:57:44 +0900

    js:강의3.데이터타입

--- js/JavaScript/javascript/variable.js
+++ js/JavaScript/javascript/variable.js
@@ -33,3 +33,23 @@ console.log(globalName);
     var age;
 }
 console.log(age);
+
+// 3. Constant
+const daysInWeek = 7;
+const maxNumber = 5;
+
+
+// 4. Variable types
+const count = 17;  //integer
+const size = 17.1; //decimal number
+console.log(`value: ${count}, type: ${typeof count}`);
+console.log(`value: ${size}, type: ${typeof size}`);
+
+
+//number - special numeric values : infinity, -infinity, NaN
+const infinity = 1 / 0;
+const negativeInfinity = -1 / 0;
+const nAn = 'not a number' / 2;
+console.log(infinity);
+console.log(negativeInfinity);
+console.log(nAn);
