742ef39acd63572dad9ec9a5daab43a8f3fa15fc
haru <jiaiha@naver.com>
2022-02-19 23:59:40 +0900

    js:4강완료

--- js/JavaScript/javascript/operator.js
+++ js/JavaScript/javascript/operator.js
@@ -126,12 +126,55 @@ switch (browser) {
         console.log('go away!');
         break;
     case 'Chrome':
-        console.log('love you!');
-        break;
     case 'Firefox':
         console.log('love you!');
         break;
+    // case 'Firefox':
+    //     console.log('love you!');
+    //     break;
     default:
         console.log('same all!');
         break;
+}
+
+// 11. Loops
+// while loop, whiel the condition is truthy,
+// body code is executed.
+let i = 3;
+while (i>0) {
+    console.log(`while: ${i}`);
+    i--;
+}
+
+// do while loop, body code is executed first,
+// then check the condition.
+do {
+    console.log(`do while: ${i}`);
+    i--;
+} while (i>0);
+
+// for loop, for(begin; condition; step)
+for (i = 3 ; i > 0 ; i--) {
+    console.log(`for: ${i}`);
+}
+
+for (let i = 3; i > 0; i = i - 2) {
+    // inline variable declaration
+    console.log(`inline variable for: ${i}`)
+}
+
+// nested loops
+for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+        console.log(`i: ${i}, j:${j}`);
+    }
+}
+
+// break, continue
+// 1. iterate from 0 to 10 and print only even numbers (use continue)
+for (let i = 0; i < 11, i++) {
+    if (i % 2 !== 0) {
+        continue;
+    }
+    console.log(`q1. ${i}`);
 }
