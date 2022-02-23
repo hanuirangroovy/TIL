be05687df0f1aa616369e9e2098d05dae736781e
haru <jiaiha@naver.com>
2022-02-23 23:57:58 +0900

    js:5강code작성완료

--- js/JavaScript/javascript/function.js
+++ js/JavaScript/javascript/function.js
@@ -78,3 +78,67 @@ function upgradeUser(user) {
     }
     // long upgrade logic...
 }
+
+// First-class function
+// functions are treated like any other variable
+// can be assigned as a value to variable
+// can be passed as an argument to other functions.
+// can be returned by another function
+
+// 1. Function expression
+// a function declaration can be called earlier than it is defined. (hoiseted)
+// a function expression is created when the execution reaches it.
+const print = function () { // anonymous function
+    console.log('print');
+};
+print();
+const printAgain = print;
+printAgain();
+const sumAgain = sum;
+console.log(sumAgain(1, 3));
+
+// 2. Callback function using fuction expression
+function randomQuiz(answer, printYes, printNo) {
+    if (answer === 'love you') {
+        printYes();
+    } else {
+        printNo();
+    }
+}
+
+const printYes = function () {
+    console.log('yes!');
+};
+
+// named function
+// better debugging in debugger's stack traces
+// recursions
+
+const printNo = function print() {
+    console.log('no!');
+};
+randomQuiz('wrong', printYes, printNo);
+randomQuiz('love you', printYes, printNo);
+
+
+// Arrow function
+// always anonymous
+const simplePrint = function () {
+    console.log('simplePrint!');
+};
+
+// const simplePrint = () => console.log('simplePrint!');
+// const add = (a,b) => a + b;
+// const simpleMultiply = (a,b) => {
+//     // do somethig more
+//     return a * b;
+// };
+
+// IIFE: Immediately Invoked Function Expression
+(function hello() {
+    console.log('IIFE');
+})();
+
+// Quiz!!
+// function calculate(command, a, b)
+// command: add, substract, divide, multiply, remainder
