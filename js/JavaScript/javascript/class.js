7fa0e7d984c9bc35f5ac9ddca7ade3a781a93fc7
haru <jiaiha@naver.com>
2022-02-26 14:51:44 +0900

    js:6강 class?

---
+++ js/JavaScript/javascript/class.js
@@ -0,0 +1,23 @@
+'use strict';
+// Object-oriendted programming
+// class: template
+// object: instance of a class
+// JavaScript classes
+//  - introduced in ES6
+//  - syntactical sugar over prototype-based inheritance
+
+// 1. Class declarations
+class Person {
+    // constructor
+    constructor(name, age) {
+        // fields
+        this.name = name;
+        this.age = age;
+    }
+
+    // methods
+    speak() {
+        console.log(`${this.name}: hello!`);
+    }
+}
+
