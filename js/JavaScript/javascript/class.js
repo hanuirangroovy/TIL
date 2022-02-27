14ed8a7b34c5b37bb3c68b2794d25a66bb279f76
haru <jiaiha@naver.com>
2022-02-27 23:56:44 +0900

    js:6강 getternsetter

--- js/JavaScript/javascript/class.js
+++ js/JavaScript/javascript/class.js
@@ -21,3 +21,30 @@ class Person {
     }
 }
 
+const ellie = new Person('ellie', 20);
+console.log(ellie.name);
+console.log(ellie.age);
+ellie.speak();
+
+// 2. Getter and setters
+class User {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    set age(value) {
+        // if (value < 0) {
+        //     throw Error('age can be negative');
+        // }
+        this._age = value < 0 ? 0 : value;
+    }
+}
+
+const user1 = new User('Steve', 'Job', -1);
+console.log(user1.age);
