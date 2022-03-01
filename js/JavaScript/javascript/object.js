2b6e666578308a45be920fe3edb2258c4777479d
haru <jiaiha@naver.com>
2022-03-01 23:59:09 +0900

    js:7강 object

---
+++ js/JavaScript/javascript/object.js
@@ -0,0 +1,15 @@
+// Objects
+// one of the JavaScript's data types.
+// a collection of related data and/or functionality.
+// Nearly all objects in JavaScript are instances of Object
+
+
+const obj1 = {}; // 'object literal' syntax
+const obj2 = new Object(); // 'object constructor' syntax
+function print(person) {
+    console.log(person.name);
+    console.log(person.age);
+}
+
+const ellie = { name: 'ellie', age: 4 };
+print(ellie);
