771406f6415ae3c3aef676413751e5973a4b0ca0
haru <jiaiha@naver.com>
2022-03-10 23:33:23 +0900

    js:9강 quiz4

--- js/JavaScript/javascript/array-api.js
+++ js/JavaScript/javascript/array-api.js
@@ -1,21 +1,30 @@
 // Q1. make a string out of an array
 {
   const fruits = ['apple', 'banana', 'orange'];
+  const result = fruits.join(',');
+  console.log(result);
 }
 
 // Q2. make an array out of a string
 {
   const fruits = '🍎, 🥝, 🍌, 🍒';
+  const result = fruits.split(',');
+  console.log(result);
 }
 
 // Q3. make this array look like this: [5, 4, 3, 2, 1]
 {
   const array = [1, 2, 3, 4, 5];
+  const result = array.reverse();
+  console.log(result);
 }
 
 // Q4. make new array without the first two elements
 {
   const array = [1, 2, 3, 4, 5];
+  const result = array.splice(0, 2);
+  console.log(result);
+  console.log(array);
 }
 
 class Student {
