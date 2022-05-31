fcdf9b894e72a2558356735838a4084cc9f95445
haru <jiaiha@naver.com>
2022-05-31 23:35:14 +0900

    TIL/js/Deepdive/03.자바스크립트 개발 환경과 실행방법

---
+++ js/DeepDive/03/index.js
@@ -0,0 +1,13 @@
+const arr = [1, 2, 3];
+
+// 3.4.2 내장터미널
+//arr.forEach(console.log);
+
+// $ node index
+// 1 0 [ 1, 2, 3 ]
+// 2 1 [ 1, 2, 3 ]
+// 3 2 [ 1, 2, 3 ]
+
+// 3.4.3 Code Runner 확장 플러그인
+// arr.forEach(alert);
+// ReferenceError: alert is not defined
