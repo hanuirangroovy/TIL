80202c94615169f4a0c07b340998a80c4cb69df9
haru <jiaiha@naver.com>
2022-10-19 23:55:28 +0900

    TIL/C#/7.4

---
+++ C#/이것이C#이다/7/StaticField/StaticField/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+class Global
+{
+    public static int Count = 0;
+}
+
+class classA
+{
+    public classA()
+    {
+        Global.Count++;
+    }
+}
+
+class classB
+{
+    public classB()
+    {
+        Global.Count++;
+    }
+}
+
+class MainApp
+{
+    static void Main()
+    {
+        Console.WriteLine($"Global.Count : {Global.Count}");
+
+        new classA();
+        new classA();
+        new classB();
+        new classB();
+
+        Console.WriteLine($"Global.Count : {Global.Count}");
+    }
+}
