777f9ade65eda0b17751e33d18b58641839713e6
haru <jiaiha@naver.com>
2022-10-29 07:40:23 +0900

    TIL/C#

---
+++ C#/CRUDusingSQL_storedprocedure_Dapper/StudentsRecordManagenentApp/StudentsRecordManagenentApp/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentsRecordManagenentApp.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? FullName { get; set; }
+        public string? EmailAddress { get; set; }
+        public string? City { get; set; }
+        public DateTime CreateOn { get; set; }
+
+            
+    }
+}
---
+++ C#/CRUDusingSQL_storedprocedure_Dapper/StudentsRecordManagenentApp/StudentsRecordManagenentApp/Services/StudentServices.cs
@@ -0,0 +1,50 @@
+﻿using Dapper;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Data.SqlClient;
+using StudentsRecordManagenentApp.Models;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+
+namespace StudentsRecordManagenentApp.Services
+{
+    public class StudentServices : IStudentService
+    {
+        private readonly IConfiguration _configuration;
+        public StudentServices(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            ConnectionString = _configuration.GetConnectionString("DBConnection");
+            providerName = "System.Data.SqlClient";
+        }
+
+        public string ConnectionString { get; }
+        public string providerName { get; }
+    
+    public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(ConnectionString);
+            }
+        }
+
+        public string InsertStudent(Student model)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+                var student = dbConnection.Query<List<Student>>("InsertStudent", model, commandType: CommandType.StoredProcedure);
+                var topRow = student.FirstOrDefault();
+                dbConnection.Close();
+            }
+            return "";
+        }
+    
+    }
+
+    public interface IStudentService
+    { 
+        
+    }
+}
