3594fdd9d53cc15488f7bf916326af3e9d8a279d
haru <jiaiha@naver.com>
2022-11-02 23:27:42 +0900

    TIL/C#/7.11,7.12

---
+++ C#/이것이C#이다/7/MethodHiding/MethodHiding/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MethodHiding
+{
+    class Base
+    {
+        public void MyMethod()
+        {
+            Console.WriteLine("Base.MyMethod()");
+        }
+    }
+
+    class Derived : Base
+    {
+        public new void MyMethod()
+        {
+            Console.WriteLine("Derived.MyMethod()");
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Base BaseObj = new Base();
+            BaseObj.MyMethod();
+
+            Derived derivedObj = new Derived();
+            derivedObj.MyMethod();
+
+            Base baseOrDerived = new Derived();
+            baseOrDerived.MyMethod();
+
+        }
+    }
+}
---
+++ C#/이것이C#이다/7/SealedMethod/SealedMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Base
+{
+    public virtual void SealMe()
+    { 
+    }
+}
+class Derived : Base {
+    public sealed override void SealMe()
+    { 
+    }
+}
+
+class WantToOverride : Derived
+{
+    public override void SealMe() // 'WantToOverride.SealMe()': 상속된 'Derived.SealMe()' 멤버는 봉인되어 있으므로 재정의할 수 없습니다.
+
+    { 
+    }
+}
+
+class MainApp
+{
+    static void Main(string[] args)
+    { 
+    }
+}
