8eb70c327aea5437746d1c701964a370c000dfde
haru <jiaiha@naver.com>
2022-11-09 23:45:22 +0900

    TIL/react/mypjt

--- react/pjt_mybooks/my-books/src/components/Signin.tsx
+++ react/pjt_mybooks/my-books/src/components/Signin.tsx
@@ -1,14 +1,42 @@
-import {Col, Row} from 'antd';
+import {Button, Col, Input, Row} from 'antd';
+import styles from './Signin.module.css';
+
 export default function Signin(){
     return (
-        <Row align='middle'>
+        <Row align='middle' className={styles.signin_row}>
             
         <Col span={24}>
-        <Row>
-            <Col span={12}></Col>
+        <Row className={styles.signin_contents}>
             <Col span={12}>
-                <div> My books </div>
-                <div>Please Note Your Opinion</div>
+                <img src="bg_signin.png" className={styles.signin_bg}></img>
+            </Col>
+            <Col span={12}>
+                <div className={styles.signin_title}> My books </div>
+                <div className={styles.signin_subtitle}>Please Note Your Opinion</div>
+                <div className={styles.signin_underline} />
+                <div className={styles.email_title}>Email
+                    <span className={styles.required}>*</span>
+            </div>
+            <div className={styles.input_area}>
+            <Input
+            placeholder="Email"
+            autoComplete="email"
+            name="email"
+            className={styles.input} />
+            </div>
+            <div className={styles.password_title}>Password
+                    <span className={styles.required}>*</span>
+            </div>
+            <div className={styles.input_area}>
+            <Input
+            type="password"
+            autoComplete="current-password"
+            name="email"
+            className={styles.input} />
+            </div>
+            <div className={styles.button_area}>
+                <Button size="large" className={styles.button}>Sign In</Button>
+            </div>
             </Col>
 
         </Row>
