8a3ed7ba3066e616d8301559c689f46e8a51d132
haru <jiaiha@naver.com>
2022-11-11 00:34:39 +0900

    TIL/react/dragndrop

--- react/dragndrop/client/src/App.js
+++ react/dragndrop/client/src/App.js
@@ -36,6 +36,22 @@ function App() {
 
   function handleDragEnd(event) {
     console.log("Drag end called");
+    const { active, over } = event;
+    console.log("ACTIVE:" + active.id);
+    console.log("OVER" + over.id);
+
+    if (active.id !== over.id) {
+      setLanguages((items) => {
+        const activeIndex = items.indexOf(active.id);
+        const overIndex = items.indexOf(over.id);
+
+        return arrayMove(items, activeIndex, overIndex);
+        console.log(arrayMove(items, activeIndex, overIndex));
+
+        arrayMove();
+        // items: [1, 2, 3]
+      });
+    }
   }
 }
 
