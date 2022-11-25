65b2c7074fc162ce0c19f36eec331b1e78e983b1
haru <jiaiha@naver.com>
2022-11-25 19:30:12 +0900

    TIL/C#

---
+++ C#/이것이C#이다/8/Interface/Interface/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Interface
+{
+    interface ILogger // ConsoleLogger와 FileLogger는 ILogger를 상속하며, WriteLog() 메소드를 구현함.
+    {
+        void WriteLog(string message);    
+    }
+
+    class ConsoleLogger : ILogger
+    {
+        public void WriteLog(string message)
+        {
+            Console.WriteLine(
+                "{0} {1}",
+                DateTime.Now.ToLocalTime(), message);
+        }
+    }
+
+    class FileLogger : ILogger 
+    {
+        private StreamWriter writer;
+
+        public FileLogger(string path)
+        {
+            writer = File.CreateText(path);
+            writer.AutoFlush = true;
+        }
+
+        public void WriteLog(string message)
+        {
+            writer.WriteLine("{0} {1}", DateTime.Now.ToShortTimeString(), message);
+        }
+    }
+
+    class ClimateMonitor
+    {
+        private ILogger logger;
+        public ClimateMonitor(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public void start()
+        {
+            while (true)
+            {
+                Console.Write("온도를 입력해주세요.:");
+                string temperature = Console.ReadLine();
+                if (temperature == "")
+                    break;
+
+                logger.WriteLog("현재 온도 : " + temperature);
+            }
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            ClimateMonitor monitor = new ClimateMonitor( // monitor 객체는 애플리케이션이 시작된 디렉터리에 MyLog.txt를 만들고 여기에 로그를 남김
+                new FileLogger("MyLog.txt"));
+
+            monitor.start();
+        }
+    }
+}
