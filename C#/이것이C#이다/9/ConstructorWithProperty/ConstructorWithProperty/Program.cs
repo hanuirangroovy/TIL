eeaa3840ecaaabb917328a97d92a2940ca37d60c
haru <jiaiha@naver.com>
2022-12-14 21:53:27 +0900

    TIL/C#/9.4

---
+++ C#/이것이C#이다/9/ConstructorWithProperty/ConstructorWithProperty/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConstructorWithProperty
+{
+    class BirthdayInfo
+    { 
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public DateTime Birthday
+        {
+            get;
+            set;
+        }
+
+        public int Age
+        {
+            get
+            {
+                return new DateTime(DateTime.Now.Subtract(Birthday).Ticks).Year;
+            }
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            BirthdayInfo birth = new BirthdayInfo()
+            {
+                Name = "지애",
+                Birthday = new DateTime(1991, 9, 10)
+            };
+
+            Console.WriteLine($"Name : {birth.Name}");
+            Console.WriteLine($"Birthday : {birth.Birthday.ToShortDateString()}");
+            Console.WriteLine($"Age : {birth.Age}");
+        }
+    }
+}
