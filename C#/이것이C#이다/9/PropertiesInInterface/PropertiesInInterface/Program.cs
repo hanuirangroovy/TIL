ab1ef4fe1164e0a35443bde9e129a40a61803f79
haru <jiaiha@naver.com>
2023-01-01 18:12:07 +0900

    TIL/C#/9.8

---
+++ C#/이것이C#이다/9/PropertiesInInterface/PropertiesInInterface/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace PropertiesInInterface
+{
+    interface INamedValue
+    { 
+        string Name
+        {
+            get;
+            set;
+        }
+
+        string Value
+        {
+            get;
+            set;
+        }
+    }
+
+    class NamedValue : INamedValue
+    { 
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Value
+        {
+            get;
+            set;
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            NamedValue name = new NamedValue()
+            { Name = "이름", Value = "하지애" };
+
+            NamedValue height = new NamedValue()
+            { Name = "키", Value = "166cm" };
+            NamedValue weight = new NamedValue()
+            { Name = "몸무게", Value = "47kg" };
+
+            Console.WriteLine($"{name.Name} : {name.Value}");
+            Console.WriteLine($"{height.Name} : {height.Value}");
+            Console.WriteLine($"{weight.Name} : {weight.Value}");
+        }
+    }
+}
