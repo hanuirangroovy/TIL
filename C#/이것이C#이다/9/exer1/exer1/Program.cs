ce66811afa0bf6f40cfc033611febd471bbf10c2
haru <jiaiha@naver.com>
2023-01-06 23:09:06 +0900

    TIL/C#/Exer1

---
+++ C#/이것이C#이다/9/exer1/exer1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ex9_1
+{
+    class NameCard
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            NameCard MyCard = new NameCard();
+
+            MyCard.Age = 32;
+            MyCard.Name = "지애";
+
+            Console.WriteLine("나이 : {0}", MyCard.Age);
+            Console.WriteLine("이름 : {0}", MyCard.Name);
+        }
+    }
+}
